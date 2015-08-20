623179a1d3b676014f582ed81d3ed3f629c3f5b4
Li Bin <binblee@users.noreply.github.com>
2015-08-20 10:17:20 +0800

    initial commit

---
+++ nodes/nodes-service/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8081;
+
+var router = express.Router();
+
+router.get('/', function(req,res){
+    res.json({greeting: 'from API'});
+});
+
+app.use('/v1', router);
+app.listen(port);
+console.log('server listen on port ' + port);
